191402800266244649a0290ebd2a28f9383a701b
Nikko <nikkosolon24@gmail.com>
2020-04-01 23:16:22 -0700

    Added to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <iostream>
-
+#include <vector>
 
 int main(){
     std::cout << "Hello World" << std::endl;
+    std::vector<int> v = {2,3,4,5,6,7};
+    for(int i = 0; i < v.size(); i++){
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
