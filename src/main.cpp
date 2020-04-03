07f31ba66f5e1f8d2f6a26027763cb5a3d2a4ee4
Nikko <nikkosolon24@gmail.com>
2020-04-02 22:35:28 -0700

    Played with random numbers

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,43 @@
 #include <stdio.h>
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
+#define SEED 420
+#define RANGE 100
+#define MY_VECTOR_SIZE 100
 
 int main(){
+    clock_t time;
+
+    std::srand(SEED);
+    
     std::cout << "Hello World" << std::endl;
-    std::vector<int> v = {2,3,4,5,6,7};
-    for(int i = 0; i < v.size(); i++){
-        std::cout << v[i] << " ";
+    
+    time = std::clock();
+    std::vector<int> v = {};
+    for(int i = 0; i < MY_VECTOR_SIZE; i++){
+        v.push_back(std::rand() % RANGE);
+        std::cout << v[i] << "\t";
     }
     std::cout << std::endl;
+    time = std::clock() - time;
+    std::cout << "Processing time: " << ((float) time)/CLOCKS_PER_SEC << std::endl;
+
+    // A container for sampling
+    std::vector<int> container;
+    // Inputing numbers into container
+    time = std::clock();
+    for(int i = 1; i < MY_VECTOR_SIZE + 1; i++){
+        container.push_back(i);
+        std::cout << container[i-1] << "\t";
+    }
+    std::cout << std::endl;
+    time = std::clock() - time;
+    std::cout << "Processing time: " << ((float) time)/CLOCKS_PER_SEC << std::endl;
+    
+    // time = std::clock();
+    // std::shuffle(container.begin(), container.end(), container);
+
     return 0;
 }
