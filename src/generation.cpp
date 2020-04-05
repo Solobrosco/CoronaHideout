b88b63a236ebc63c5625db78650a6bb5fecf772c
Nikko <nikkosolon24@gmail.com>
2020-04-04 22:46:24 -0700

    Added generation.cpp

---
+++ src/generation.cpp
@@ -0,0 +1,14 @@
+#include <olcPixelGameEngine.h>
+#include <iostream>
+
+class Verse{
+protected: 
+    int size;
+public:
+
+}
+
+int main(){
+    std::cout << "Generation test" << std::endl;
+    return 0;
+}
